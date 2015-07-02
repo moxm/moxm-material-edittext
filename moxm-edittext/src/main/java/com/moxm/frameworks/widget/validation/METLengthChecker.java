27610826393898d1bb06bfe4677cdc7aba45fcbc
马祥力 <xiangli.ma@newtouch.cn>
2015-07-02 16:59:20 +0800

    Initial commit

---
+++ moxm-edittext/src/main/java/com/moxm/frameworks/widget/validation/METLengthChecker.java
@@ -0,0 +1,10 @@
+package com.moxm.frameworks.widget.validation;
+
+/**
+ * Created by Richard.Ma on 15/4/12.
+ */
+public abstract class METLengthChecker {
+
+    public abstract int getLength(CharSequence text);
+
+}
